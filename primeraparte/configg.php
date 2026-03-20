5211f0b5a5b0f60b6b0825b6bef39b8064efa358
jorgearios <2230492@upv.edu.mx>
2026-03-20 02:48:41 -0600

    Actualizacion del config

--- primeraparte/configg.php
+++ primeraparte/configg.php
@@ -1,11 +1,12 @@
 <?php
 
-$host = '127.0.0.1';
-$user = 'admin';
-$pass = 'e11a5294076c2dca0c0117243bb6dcff951d5cb5da79f0ca';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
 $db = 'sistema_gym';
 
 $conexion = new mysqli($host, $user, $pass, $db);
+
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
@@ -14,4 +15,6 @@ if ($conexion->connect_error) {
 $conexion->set_charset("utf8mb4");
 
 
+// Incluimos el control de sesiones y protección de páginas automáticamente
+require_once 'auth.php';
 ?>
